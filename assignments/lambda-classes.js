72492b292fa074e915c387e9bcee937399ae1591
Germaine Pannell <me@germainep.com>
2019-08-18 15:39:02 -0400

    added classes for Person, Instructor and Student.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,49 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+
+  speak() {
+    console.log(`Hello, my name is ${this.name}, I'm from ${this.location}`);
+  }
+}
+class Instructor extends Person {
+  constructor(attributes) {
+    super(attributes);
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+  }
+
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}`);
+  }
+
+  grade(student, subject) {
+    console.log(`${student.name} recieves a perfect score on ${subject}`);
+  }
+}
+
+class Student extends Person {
+  constructor(attributes) {
+    super(attributes);
+    this.previousBackground = attributes.previousBackground;
+    this.className = attributes.className;
+    this.favSubjects = attributes.favSubjects;
+  }
+
+  listsSubjects() {
+    this.favSubjects.forEach(item => console.log(item));
+  }
+
+  PRAssignment(subject) {
+    console.log(`${this.name} has submitted a PR for ${subject}`);
+  }
+
+  sprintChallenge(subject) {
+    console.log(`${this.name} has begun sprint challenge on ${subject}`);
+  }
+}
