9f5ec3f7809511660e1daccb533d0c6d4fc68bf6
Germaine Pannell <me@germainep.com>
2019-08-18 16:21:07 -0400

    completed lambda-classes.js
    manually test the use cases for the file

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -49,17 +49,61 @@ class Student extends Person {
 }
 
 class ProjectManager extends Person {
-  construtor(attributes) {
+  constructor(attributes) {
     super(attributes);
     this.gradClassName = attributes.gradClassName;
     this.favInstructor = attributes.favInstructor;
   }
 
   standup(channel) {
-    console.log(`${this.name} announces to ${channel} @channel standup time`)
+    console.log(`${this.name} announces to ${channel} @channel standup time`);
   }
 
-  debugsCode({student}, subject) {
-    console.log(`${this.name} debugs ${student.name}'s code ${subject}`)
+  debugsCode(student, subject) {
+    console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
   }
 }
+
+const john = new Student({
+  name: 'John',
+  age: 25,
+  location: 'Petersburg',
+  previousBackground: 'welder',
+  className: 'CS1',
+  favSubjects: ['javascript', 'databases', 'ui'],
+});
+
+const sam = new Instructor({
+  name: 'Sam',
+  age: 50,
+  location: 'Houston',
+  specialty: 'HTML',
+  favLanguage: 'Clojure',
+  catchPhrase: 'Get up with the get up',
+});
+
+const boe = new ProjectManager({
+  name: 'Boe',
+  age: 35,
+  location: 'Atlanta',
+  gradClassName: 'UX54',
+  favInstructor: 'Sam',
+});
+
+console.log(john);
+console.log(sam);
+console.log(boe);
+
+sam.speak();
+john.speak();
+boe.speak();
+
+john.listsSubjects();
+john.PRAssignment('javascript-I');
+john.sprintChallenge('javascript-II');
+
+sam.demo('javascript-I');
+sam.grade(john, 'javascript-IV');
+
+boe.standup('slack-slack');
+boe.debugsCode(john, 'javascript-IV');
